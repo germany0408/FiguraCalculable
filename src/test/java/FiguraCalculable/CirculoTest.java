9806a8bf1411bd0947a2e3791510cb3e34033472
german <germany0408@hotmail.com>
2013-08-10 20:59:22 -0500

    SOn las figuras

---
+++ src/test/java/FiguraCalculable/CirculoTest.java
@@ -0,0 +1,34 @@
+package FiguraCalculable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: German
+ * Date: 10/08/13
+ * Time: 08:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CirculoTest {
+    public void testArea() throws Exception {
+
+    }
+
+    public void testPerimetro() throws Exception {
+
+    }
+
+    public void testGetRadio() throws Exception {
+
+    }
+
+    public void testSetRadio() throws Exception {
+
+    }
+
+    public void testGetDiametro() throws Exception {
+
+    }
+
+    public void testSetDiametro() throws Exception {
+
+    }
+}
